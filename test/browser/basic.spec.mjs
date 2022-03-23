fea00063ef09b67792a7614b013b5c138d69a690
Ryan Bethel <ryan.bethel@gmail.com>
2022-03-22 21:11:17 -0400

    fixed test

--- test/browser/basic.spec.mjs
+++ test/browser/basic.spec.mjs
@@ -5,19 +5,19 @@ test('basic mock login', async ({ page }) => {
   await page.goto(`${baseUrl}`)
   const login = await page.locator('text=Github')
   await login.click()
-  const user = await page.locator('text=John Doe')
+  const user = await page.locator('text=Jane Doe')
   await user.click()
   const message = await page.locator('text=Hello')
   const text = await message.innerText()
 
-  await expect(text).toBe('Hello John Doe You Made it!')
+  await expect(text).toBe('Hello Jane Doe You Made it!')
 })
 
 test('basic mock logout', async ({ page }) => {
   await page.goto(`${baseUrl}`)
   let login = await page.locator('text=Github')
   await login.click()
-  const user = await page.locator('text=John Doe')
+  const user = await page.locator('text=Jane Doe')
   await user.click()
   const logout = await page.locator('text=logout')
   await logout.click()
